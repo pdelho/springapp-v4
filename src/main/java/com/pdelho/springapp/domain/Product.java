294cb47f8cba324f87174388a06dc68b6d11dc02
pablo <pablo@LAPTOP-KHGRKL6A.home>
2017-03-06 21:38:01 +0100

    Migration to PostgreSQL working

--- src/main/java/com/pdelho/springapp/domain/Product.java
+++ src/main/java/com/pdelho/springapp/domain/Product.java
@@ -10,7 +10,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="products") 
+@Table(name="product") 
 public class Product implements Serializable {
 
     private static final long serialVersionUID = 1L;
--- src/test/java/com/pdelho/springapp/web/InventoryControllerTests.java
+++ src/test/java/com/pdelho/springapp/web/InventoryControllerTests.java
@@ -1,27 +1,33 @@
 package com.pdelho.springapp.web;
 
+import java.util.ArrayList;
+import java.util.Map;
+
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 import org.springframework.web.servlet.ModelAndView;
-import java.util.Map;
+
+import com.pdelho.springapp.domain.Product;
+import com.pdelho.springapp.repository.InMemoryProductDao;
 import com.pdelho.springapp.service.SimpleProductManager;
 
 
 public class InventoryControllerTests {
 
-    
-	@Test
+    @Test
     public void testHandleRequestView() throws Exception{		
         InventoryController controller = new InventoryController();
-        controller.setProductManager(new SimpleProductManager());
+        SimpleProductManager spm = new SimpleProductManager();
+        spm.setProductDao(new InMemoryProductDao(new ArrayList<Product>()));
+        controller.setProductManager(spm);
+        //controller.setProductManager(new SimpleProductManager());
         ModelAndView modelAndView = controller.handleRequest(null, null);		
         assertEquals("hello", modelAndView.getViewName());
         assertNotNull(modelAndView.getModel());
-        @SuppressWarnings("unchecked")
-        Map<String, Object> modelMap = (Map<String, Object>) modelAndView.getModel().get("model");
+        @SuppressWarnings("rawtypes")
+		Map modelMap = (Map) modelAndView.getModel().get("model");
         String nowValue = (String) modelMap.get("now");
         assertNotNull(nowValue);
     }
-
 }
